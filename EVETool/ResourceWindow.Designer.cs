596c71290b0aed06e64891fb0583ae67c47a2da7
Jeremy <xboxmusicn3rd@gmail.com>
2012-08-17 02:53:41 -0700

    Planet Selection UI

--- EVETool/ResourceWindow.Designer.cs
+++ EVETool/ResourceWindow.Designer.cs
@@ -30,8 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResourceWindow));
             this.ResourcePanel = new System.Windows.Forms.Panel();
-            this.NewIndustryInter = new System.Windows.Forms.Button();
             this.SetSize = new System.Windows.Forms.Panel();
+            this.Temperate = new System.Windows.Forms.PictureBox();
+            this.Storm = new System.Windows.Forms.PictureBox();
+            this.Plasma = new System.Windows.Forms.PictureBox();
+            this.Oceanic = new System.Windows.Forms.PictureBox();
+            this.Lava = new System.Windows.Forms.PictureBox();
+            this.Ice = new System.Windows.Forms.PictureBox();
+            this.Gas = new System.Windows.Forms.PictureBox();
+            this.Barren = new System.Windows.Forms.PictureBox();
+            this.NewIndustryInter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Temperate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Storm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plasma)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Oceanic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lava)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Barren)).BeginInit();
             this.SuspendLayout();
             // 
             // ResourcePanel
@@ -39,25 +55,104 @@
             resources.ApplyResources(this.ResourcePanel, "ResourcePanel");
             this.ResourcePanel.Name = "ResourcePanel";
             // 
-            // NewIndustryInter
-            // 
-            resources.ApplyResources(this.NewIndustryInter, "NewIndustryInter");
-            this.NewIndustryInter.Name = "NewIndustryInter";
-            this.NewIndustryInter.UseVisualStyleBackColor = true;
-            this.NewIndustryInter.Click += new System.EventHandler(this.NewIndustryInter_Click);
-            this.NewIndustryInter.BackgroundImage = global::EVETool.Properties.Resources._33_128_2;
-          
-            // 
             // SetSize
             // 
             resources.ApplyResources(this.SetSize, "SetSize");
             this.SetSize.Name = "SetSize";
+            // 
+            // Temperate
+            // 
+            this.Temperate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Temperate.Image = global::EVETool.Properties.Resources.Temperate;
+            resources.ApplyResources(this.Temperate, "Temperate");
+            this.Temperate.Name = "Temperate";
+            this.Temperate.TabStop = false;
+            this.Temperate.Click += new System.EventHandler(this.Temperate_Click);
+            // 
+            // Storm
+            // 
+            this.Storm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Storm.Image = global::EVETool.Properties.Resources.Storm;
+            resources.ApplyResources(this.Storm, "Storm");
+            this.Storm.Name = "Storm";
+            this.Storm.TabStop = false;
+            this.Storm.Click += new System.EventHandler(this.Storm_Click);
+            // 
+            // Plasma
+            // 
+            this.Plasma.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Plasma.Image = global::EVETool.Properties.Resources.Plasma;
+            resources.ApplyResources(this.Plasma, "Plasma");
+            this.Plasma.Name = "Plasma";
+            this.Plasma.TabStop = false;
+            this.Plasma.Click += new System.EventHandler(this.Plasma_Click);
+            // 
+            // Oceanic
+            // 
+            this.Oceanic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Oceanic.Image = global::EVETool.Properties.Resources.Oceanic;
+            resources.ApplyResources(this.Oceanic, "Oceanic");
+            this.Oceanic.Name = "Oceanic";
+            this.Oceanic.TabStop = false;
+            this.Oceanic.Click += new System.EventHandler(this.Oceanic_Click);
+            // 
+            // Lava
+            // 
+            this.Lava.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Lava.Image = global::EVETool.Properties.Resources.Lava;
+            resources.ApplyResources(this.Lava, "Lava");
+            this.Lava.Name = "Lava";
+            this.Lava.TabStop = false;
+            this.Lava.Click += new System.EventHandler(this.Lava_Click);
+            // 
+            // Ice
+            // 
+            resources.ApplyResources(this.Ice, "Ice");
+            this.Ice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Ice.Image = global::EVETool.Properties.Resources.Ice;
+            this.Ice.Name = "Ice";
+            this.Ice.TabStop = false;
+            this.Ice.Click += new System.EventHandler(this.Ice_Click);
+            // 
+            // Gas
+            // 
+            resources.ApplyResources(this.Gas, "Gas");
+            this.Gas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Gas.Image = global::EVETool.Properties.Resources.Gas;
+            this.Gas.Name = "Gas";
+            this.Gas.TabStop = false;
+            this.Gas.Click += new System.EventHandler(this.Gas_Click);
+            // 
+            // Barren
+            // 
+            resources.ApplyResources(this.Barren, "Barren");
+            this.Barren.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Barren.Image = global::EVETool.Properties.Resources.Barren;
+            this.Barren.Name = "Barren";
+            this.Barren.TabStop = false;
+            this.Barren.Click += new System.EventHandler(this.Barren_Click);
+            // 
+            // NewIndustryInter
+            // 
+            this.NewIndustryInter.BackgroundImage = global::EVETool.Properties.Resources._33_128_2;
+            resources.ApplyResources(this.NewIndustryInter, "NewIndustryInter");
+            this.NewIndustryInter.Name = "NewIndustryInter";
+            this.NewIndustryInter.UseVisualStyleBackColor = true;
+            this.NewIndustryInter.Click += new System.EventHandler(this.NewIndustryInter_Click);
             // 
             // ResourceWindow
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Controls.Add(this.Temperate);
+            this.Controls.Add(this.Storm);
+            this.Controls.Add(this.Plasma);
+            this.Controls.Add(this.Oceanic);
+            this.Controls.Add(this.Lava);
+            this.Controls.Add(this.Ice);
+            this.Controls.Add(this.Gas);
+            this.Controls.Add(this.Barren);
             this.Controls.Add(this.ResourcePanel);
             this.Controls.Add(this.NewIndustryInter);
             this.Controls.Add(this.SetSize);
@@ -65,6 +160,14 @@
             this.MaximizeBox = false;
             this.Name = "ResourceWindow";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            ((System.ComponentModel.ISupportInitialize)(this.Temperate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Storm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plasma)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Oceanic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lava)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Gas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Barren)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -74,6 +177,14 @@
         private System.Windows.Forms.Button NewIndustryInter;
         private System.Windows.Forms.Panel ResourcePanel;
         private System.Windows.Forms.Panel SetSize;
+        private System.Windows.Forms.PictureBox Barren;
+        private System.Windows.Forms.PictureBox Gas;
+        private System.Windows.Forms.PictureBox Ice;
+        private System.Windows.Forms.PictureBox Lava;
+        private System.Windows.Forms.PictureBox Oceanic;
+        private System.Windows.Forms.PictureBox Plasma;
+        private System.Windows.Forms.PictureBox Storm;
+        private System.Windows.Forms.PictureBox Temperate;
     }
 }
 
--- EVETool/ResourceWindow.cs
+++ EVETool/ResourceWindow.cs
@@ -14,6 +14,14 @@ namespace EVETool
 {
     public partial class ResourceWindow : Form
     {
+        private Boolean BarrenImg = true;
+        private Boolean GasImg = true;
+        private Boolean IceImg = true;
+        private Boolean LavaImg = true;
+        private Boolean OceanicImg = true;
+        private Boolean PlasmaImg = true;
+        private Boolean StormImg = true;
+        private Boolean TemperateImg = true;
         public ResourceWindow()
         {
             InitializeComponent();
@@ -24,6 +32,118 @@ namespace EVETool
         {
             ResourcePanel.Controls.Clear();
             ResourcePanel.Controls.Add(new ResourcePanel());
+        }
+
+        private void Barren_Click(object sender, EventArgs e)
+        {
+            if (BarrenImg == true)
+            {
+                BarrenImg = false;
+                Barren.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (BarrenImg == false)
+            {
+                BarrenImg = true;
+                Barren.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Gas_Click(object sender, EventArgs e)
+        {
+            if (GasImg == true)
+            {
+                GasImg = false;
+                Gas.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (GasImg == false)
+            {
+                GasImg = true;
+                Gas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Ice_Click(object sender, EventArgs e)
+        {
+            if (IceImg == true)
+            {
+                IceImg = false;
+                Ice.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (IceImg == false)
+            {
+                IceImg = true;
+                Ice.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Lava_Click(object sender, EventArgs e)
+        {
+            if (LavaImg == true)
+            {
+                LavaImg = false;
+                Lava.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (LavaImg == false)
+            {
+                LavaImg = true;
+                Lava.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Oceanic_Click(object sender, EventArgs e)
+        {
+            if (OceanicImg == true)
+            {
+                OceanicImg = false;
+                Oceanic.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (OceanicImg == false)
+            {
+                OceanicImg = true;
+                Oceanic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Plasma_Click(object sender, EventArgs e)
+        {
+            if (PlasmaImg == true)
+            {
+                PlasmaImg = false;
+                Plasma.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (PlasmaImg == false)
+            {
+                PlasmaImg = true;
+                Plasma.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Storm_Click(object sender, EventArgs e)
+        {
+            if (StormImg == true)
+            {
+                StormImg = false;
+                Storm.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (StormImg == false)
+            {
+                StormImg = true;
+                Storm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
+        }
+
+        private void Temperate_Click(object sender, EventArgs e)
+        {
+            if (TemperateImg == true)
+            {
+                TemperateImg = false;
+                Temperate.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            }
+            else if (TemperateImg == false)
+            {
+                TemperateImg = true;
+                Temperate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            }
         }
     }
 }
